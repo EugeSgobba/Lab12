4269be0795c0f5eb831cef80b0b595fa4ea45881
Alberto Monge <albertomonge91@gmail.com>
2018-06-11 09:25:42 +0200

    soluzione

--- Lab12_Rivers/src/it/polito/tdp/rivers/db/RiversDAO.java
+++ Lab12_Rivers/src/it/polito/tdp/rivers/db/RiversDAO.java
@@ -1,14 +1,15 @@
 package it.polito.tdp.rivers.db;
 
-import java.util.LinkedList;
-import java.util.List;
-
+import it.polito.tdp.rivers.model.Flow;
 import it.polito.tdp.rivers.model.River;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class RiversDAO {
 
@@ -36,4 +37,33 @@ public class RiversDAO {
 
 		return rivers;
 	}
+
+	public List<Flow> getFlows(River river) {
+		
+		final String sql = "SELECT id, day, flow FROM flow WHERE river=?";
+
+		List<Flow> flows = new LinkedList<Flow>();
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, river.getId());
+			ResultSet res = st.executeQuery();
+
+			while (res.next()) {
+				flows.add(new Flow(res.getDate("day").toLocalDate(), res.getDouble("flow"), river));
+			}
+			Collections.sort(flows);
+			river.setFlows(flows);
+			
+			conn.close();
+		} catch (SQLException e) {
+			//e.printStackTrace();
+			throw new RuntimeException("SQL Error");
+		}
+
+		return flows;
+	}
+
+
 }
